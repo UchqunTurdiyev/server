be74804eab1e5d76356e1a14f3e8791418e45a90
uchqun007 <uchqunjonturdiev@gmail.com>
2023-06-15 10:48:38 +0500

    auth autorisation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { Auth } from 'src/auth/decorator/auth.decorator';
+import { User } from './decorators/user.decorator';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Get('profile')
-  @Auth('ADMIN')
-  async getProdile() {
-    return 'my-profile';
+  @Auth()
+  async getProdile(@User('_id') _id: string) {
+    return this.userService.byId(_id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserModule } from './user.module';
+import { User, UserDocument } from './user.model';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async byId(id: string) {
+    const user = await this.userModel.findById(id);
+
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
+}
